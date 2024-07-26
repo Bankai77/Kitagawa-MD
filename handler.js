2f66991c407f5e72d1e7c30db83cc2023b3b5501
Bankai <sarkarka2005@gmail.com>
2024-07-26 21:40:11 +0530

    Update handler.js

--- handler.js
+++ handler.js
@@ -577,7 +577,7 @@ export async function participantsUpdate({ id, participants, action }) {
                     title: 'ᴋɪᴛᴀɢᴀᴡᴀ ᴛʜᴇ ᴀɴɪᴍᴇ ʙᴏᴛ',
                     body: 'Welcome to Group',
                     thumbnailUrl: welcomeApiUrl,
-                    sourceUrl: 'https://chat.whatsapp.com/KnOfHuspbLh4Cd0ofYGqdQ',
+                    sourceUrl: 'https://chat.whatsapp.com/Hz5C06OZVkhLQ2vA3kp6GX',
                     mediaType: 1,
                     renderLargerThumbnail: true,
                   },
