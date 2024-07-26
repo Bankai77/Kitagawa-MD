5b7da4aa49f88e45066317f09fc840d1257be482
Bankai <sarkarka2005@gmail.com>
2024-07-26 20:49:42 +0530

    Update fun-waste.js

--- plugins/fun-waste.js
+++ plugins/fun-waste.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn }) => {
         ? conn.user.jid
         : m.sender
   let name = conn.getName(who)
-  let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './Guru.jpg')
+  let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './shizo.jpg')
   conn.sendFile(
     m.chat,
     global.API('https://some-random-api.com', '/canvas/overlay/wasted', {
